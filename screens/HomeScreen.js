503efa80930e2eb71c85dab8272f12c218e947df
AkshithSRK <72197333+AkshithSRK@users.noreply.github.com>
2022-12-23 18:56:57 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image,
+    ScrollView,
+  } from 'react-native';
+  import Ionicons from 'react-native-vector-icons/Ionicons';
+  import { TouchableOpacity } from 'react-native-gesture-handler';
+  import firebase from 'firebase';
+
+export default class HomeScreen extends React.Component {
+
+constructor(){
+  super();
+  this.state = {
+    
+  }
+}
+ 
+
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ 
+});
